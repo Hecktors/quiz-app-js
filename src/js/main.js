147f3e04d846c5f59e10a68133d52d3d1213828d
Torsten <tohe.webdev@gmail.com>
2020-10-07 17:38:53 +0200

    Feature: multi mains in index.html

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+const headerTitle = document.querySelector('[data-js=headerTitle]')
+
+const homeMain = document.querySelector('[data-js=homeMain]')
+const bookmarksMain = document.querySelector('[data-js=bookmarksMain]')
+const createMain = document.querySelector('[data-js=createMain]')
+const profileMain = document.querySelector('[data-js=profileMain]')
+
+const homeNavLink = document.querySelector('[data-js=homeNavLink]')
+const bookmarksNavLink = document.querySelector('[data-js=bookmarksNavLink]')
+const createNavLink = document.querySelector('[data-js=createNavLink]')
+const profileNavLink = document.querySelector('[data-js=profileNavLink]')
+
+homeNavLink.addEventListener('click', () => {
+  hideMains()
+  showMain(homeMain)
+  setHeaderTitle('Quiz-App')
+})
+
+bookmarksNavLink.addEventListener('click', () => {
+  hideMains()
+  showMain(bookmarksMain)
+  setHeaderTitle('Bookmarks')
+})
+
+createNavLink.addEventListener('click', () => {
+  hideMains()
+  showMain(createMain)
+  setHeaderTitle('Create')
+})
+
+profileNavLink.addEventListener('click', () => {
+  hideMains()
+  showMain(profileMain)
+  setHeaderTitle('Profile')
+})
+
+function hideMains() {
+  homeMain.classList.add('d-none')
+  bookmarksMain.classList.add('d-none')
+  createMain.classList.add('d-none')
+  profileMain.classList.add('d-none')
+}
+
+function showMain(main) {
+  main.classList.remove('d-none')
+}
+
+function setHeaderTitle(string) {
+  headerTitle.innerText = string
+}
